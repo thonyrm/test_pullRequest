f156bf0bf2ae9793bfb6639149f9366d79155a1f
Anthony Ramos Morales <100253179+thonyrm@users.noreply.github.com>
2025-12-17 19:42:17 -0300

    Correct formatting and add semicolons in file1.js
    Fix formatting and add missing semicolons.

--- file1.js
+++ file1.js
@@ -1,7 +1,8 @@
 const num1  = 10 ;
 const num2 = 20; 
 const num3 = 30;
-const num4 = 15
+const num4 = 15;
+const num5= 100
 const result = num1 + num2 ;
 const result = num2 - num1 ;
 const result1 = num1 * num2 ;
